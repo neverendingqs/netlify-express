9cd0e96d7ea898675753031f2609110bb829c71b
Mark Tse <mark.tse@neverendingqs.com>
2018-08-21 22:56:51 -0400

    app - having index.html load page served by Express.js.

--- express/server.js
+++ express/server.js
@@ -5,7 +5,11 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-router.get('/', (req, res) => res.json({ route: req.originalUrl }));
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
