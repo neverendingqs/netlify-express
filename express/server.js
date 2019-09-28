b3b0897b825bd3219faf0c24c363a257944b667a
Mark Tse <neverendingqs@users.noreply.github.com>
2019-09-28 17:11:28 -0400

    app - enable local. (#13)

--- express/server.js
+++ express/server.js
@@ -1,5 +1,6 @@
 'use strict';
 const express = require('express');
+const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
@@ -15,6 +16,7 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
