b79b8d32babbbd420fccebee8c423b8859ac9da4
BaNgan <bangank36@gmail.com>
2021-09-15 19:25:08 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,5 +1,6 @@
 'use strict';
 const express = require('express');
+const { exec } = require('child_process')
 const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
@@ -16,7 +17,50 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+app.use('/npm/:package', async (req, res) => {
+    let _package = req.params.package;
+    let result = null;
+    try {
+      result = await runCommand(`npm view ${_package} versions --json`);
+    } catch(err) {
+      result = err;
+    }
+    
+    res.send(result);
+})
+
+app.use('/npm/:scoped/:package', async (req, res) => {
+    let scoped = req.params.scoped;
+    let _package = req.params.package;
+    let result = null;
+    try {
+    result = await runCommand(`npm view ${scoped}/${_package} versions --json`);
+    } catch(err) {
+    result = err;
+    }
+      
+    res.send(result);
+})
+
+app.use('/npm', async (req, res) => {
+    res.send(`Add the package name in the correct format to get all package version in JSON /npm/:packageName or /npm/:scoped/:packageName`)
+})
+
+//The 404 Route (ALWAYS Keep this as the last route)
+app.use('*', function(req, res){
+    res.status(404).send(`Add the package name in the correct format to get all package version in JSON /npm/:packageName or /npm/:scoped/:packageName`)
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
+
+function runCommand(cmd) {
+    return new Promise((resolve, reject) => {
+      exec(cmd, (error, stdout, stderr) => {
+        if (error) return reject(error)
+        if (stderr) return reject(stderr)
+        resolve(stdout)
+      })
+    })
+  }
